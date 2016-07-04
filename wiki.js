91bc663ac9e58039f2e53c8ef707611696700d82
schwarzflugel <weibflugel@naver.com>
2016-07-04 09:05:06 +0900

    0704_0905

--- wiki.js
+++ wiki.js
@@ -1,5 +1,11 @@
 //서버 기동을 위한 모듈 추가
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mongoDB');
+var wikiDB = mongoose.connection;
+
+var path = require('path');
